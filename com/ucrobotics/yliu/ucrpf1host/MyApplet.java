74c320743b4033beb5b76ffa70f15fe4eb17cd64
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2017-05-03 23:47:32 +0800

    UI refactoring.
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paulliu@debian.org>

--- com/ucrobotics/yliu/ucrpf1host/MyApplet.java
+++ com/ucrobotics/yliu/ucrpf1host/MyApplet.java
@@ -31,19 +31,117 @@ public class MyApplet extends JApplet {
      */
     public void init() {
 	Container cp = getContentPane();
-	JPanel mainPanel = null;
-	JPanel filamentPanel = null;
-	JPanel connectPanel = null;
 
 	this.logger = java.util.logging.Logger.getLogger(MyApplet.loggerName);
 	this.logger.setLevel(java.util.logging.Level.INFO);
 
-	cp.setLayout(new GridLayout(1,1));
+	cp.setLayout(new BorderLayout());
 
 	cards = new JPanel(new CardLayout());
-	cp.add(cards);
+	cp.add(cards, BorderLayout.CENTER);
 
-	mainPanel = new JPanel();
+	JPanel mainPanel = createMainPanel();
+	cards.add(mainPanel, "MainPanel");
+	
+	JPanel filamentPanel = createFilamentPanel();
+	cards.add(filamentPanel, "FilamentPanel");
+
+	JPanel connectPanel = createConnectPanel();
+	cards.add(connectPanel, "ConnectPanel");
+
+	JPanel printingInfoPanel = createPrintingInfoPanel();
+	cards.add(printingInfoPanel, "PrintingInfoPanel");
+
+	/* default card */
+	goToCard("ConnectPanel");
+    }
+
+    /**
+     * Create "PrintingInfoPanel".
+     * This function should be only called once in 
+     * init().
+     *
+     * @return: a JPanel for "PrintingInfoPanel"
+     */
+    private JPanel createPrintingInfoPanel() {
+	JPanel printingInfoPanel = new JPanel();
+	printingInfoPanel.setLayout(new BorderLayout());
+	
+	JButton backToMainButton = new JButton("Back to Main");
+	backToMainButton.addActionListener(new BackToMainButtonActionListener());
+	printingInfoPanel.add(backToMainButton, BorderLayout.SOUTH);
+
+	JPanel progressPanel = new JPanel();
+	JProgressBar progressBar = new JProgressBar();
+	progressBar.setIndeterminate(true);
+	progressPanel.add(progressBar);
+	printingInfoPanel.add(progressPanel, BorderLayout.CENTER);
+	return printingInfoPanel;
+    }
+
+    /**
+     * Create "ConnectPanel".
+     * This function should be only called once in 
+     * init().
+     *
+     * @return: a JPanel for "ConnectPanel"
+     */
+    private JPanel createConnectPanel() {
+	JPanel connectPanel = new JPanel();
+	connectPanel.setLayout(new FlowLayout());
+
+	ArrayList<String> devices = PF1Device.listDevices();
+	JComboBox deviceComboBox = new JComboBox();
+	for (String dev : devices) {
+	    deviceComboBox.addItem(dev);
+	}
+	JButton connectButton = new JButton("Connect");
+	connectButton.addActionListener(new ConnectButtonActionListener(deviceComboBox));
+	
+	connectPanel.add(deviceComboBox);
+	connectPanel.add(connectButton);
+	return connectPanel;
+    }
+    
+    /**
+     * Create "FilamentPanel".
+     * This function should be only called once in 
+     * init().
+     *
+     * @return: a JPanel for "FilamentPanel"
+     */
+    private JPanel createFilamentPanel() {
+	JPanel filamentPanel = new JPanel();
+	filamentPanel.setLayout(new BorderLayout());
+
+	JButton loadFilamentButton = new JButton("Load Filament");
+	loadFilamentButton.addActionListener(new LoadFilamentButtonActionListener());
+	
+	JButton unloadFilamentButton = new JButton("Unload Filament");
+	unloadFilamentButton.addActionListener(new UnloadFilamentButtonActionListener());
+
+	JPanel filamentPanelBox1 = new JPanel();
+	filamentPanelBox1.setLayout(new GridLayout(1,2));
+	
+	JButton backToMainButton = new JButton("Back to Main");
+	backToMainButton.addActionListener(new BackToMainButtonActionListener());
+
+	filamentPanelBox1.add(loadFilamentButton);
+	filamentPanelBox1.add(unloadFilamentButton);
+	filamentPanel.add(filamentPanelBox1, BorderLayout.CENTER);
+	filamentPanel.add(backToMainButton, BorderLayout.SOUTH);
+	return filamentPanel;
+    }
+    
+    /**
+     * Create "MainPanel".
+     * This function should be only called once in 
+     * init().
+     *
+     * @return: a JPanel for "MainPanel"
+     */
+    private JPanel createMainPanel() {
+	JPanel mainPanel = new JPanel();
 	mainPanel.setLayout(new GridLayout(2,3));
 
 	JButton printButton = new JButton("Print");
@@ -71,61 +169,29 @@ public class MyApplet extends JApplet {
 	mainPanel.add(settingsButton);
 	mainPanel.add(infoButton);
 
-	filamentPanel = new JPanel();
-	filamentPanel.setLayout(new BorderLayout());
+	return mainPanel;
+    }
 
-	JButton loadFilamentButton = new JButton("Load Filament");
-	loadFilamentButton.addActionListener(new LoadFilamentButtonActionListener());
-	
-	JButton unloadFilamentButton = new JButton("Unload Filament");
-	unloadFilamentButton.addActionListener(new UnloadFilamentButtonActionListener());
-
-	JPanel filamentPanelBox1 = new JPanel();
-	filamentPanelBox1.setLayout(new GridLayout(1,2));
-	
-	JButton backToMainButton = new JButton("Back to Main");
-	backToMainButton.addActionListener(new BackToMainButtonActionListener());
-
-	filamentPanelBox1.add(loadFilamentButton);
-	filamentPanelBox1.add(unloadFilamentButton);
-	filamentPanel.add(filamentPanelBox1, BorderLayout.CENTER);
-	filamentPanel.add(backToMainButton, BorderLayout.SOUTH);
-
-	connectPanel = new JPanel();
-	connectPanel.setLayout(new FlowLayout());
-
-	ArrayList<String> devices = PF1Device.listDevices();
-	JComboBox deviceComboBox = new JComboBox();
-	for (String dev : devices) {
-	    deviceComboBox.addItem(dev);
-	}
-	JButton connectButton = new JButton("Connect");
-	connectButton.addActionListener(new ConnectButtonActionListener(deviceComboBox));
-	
-	connectPanel.add(deviceComboBox);
-	connectPanel.add(connectButton);
-
-	cards.add(mainPanel, "MainPanel");
-	cards.add(filamentPanel, "FilamentPanel");
-	cards.add(connectPanel, "ConnectPanel");
-
-
-	/* default card */
-	((CardLayout)cards.getLayout()).show(cards, "ConnectPanel");
-
+    /**
+     * Go to the card registered by name
+     *
+     * @name: the name of the card
+     */
+    public void goToCard(String name) {
+	((CardLayout)cards.getLayout()).show(cards, name);
     }
 
     class PrintButtonActionListener implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 	    logger.info("Print");
+	    goToCard("PrintingInfoPanel");
 	}
     }
 
     class FilamentButtonActionListener implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 	    logger.info("Filament");
-	    CardLayout cl = (CardLayout)cards.getLayout();
-	    cl.show(cards, "FilamentPanel");
+	    goToCard("FilamentPanel");
 	}
     }
 
@@ -168,8 +234,7 @@ public class MyApplet extends JApplet {
     class BackToMainButtonActionListener implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 	    logger.info("Unload Filamente");
-	    CardLayout cl = (CardLayout)cards.getLayout();
-	    cl.show(cards, "MainPanel");
+	    goToCard("MainPanel");
 	}
     }
 
@@ -180,7 +245,7 @@ public class MyApplet extends JApplet {
 	}
 	public void actionPerformed(ActionEvent e) {
 	    logger.info("Connect");
-	    ((CardLayout)cards.getLayout()).show(cards, "MainPanel");
+	    goToCard("MainPanel");
 	    try {
 		pf1Device = new PF1Device((String)deviceComboBox.getSelectedItem());
 	    } catch (java.io.FileNotFoundException e1) {
